9b29ac94f4689358c615f390a9f3e49128c04502
akashtripathi1 <120573423+akashtripathi1@users.noreply.github.com>
2024-04-30 19:02:07 +0530

    created auth context , state and reducer

--- client/src/App.js
+++ client/src/App.js
@@ -4,33 +4,38 @@ import Navbar from './components/layout/Navbar';
 import Home from './components/pages/Home';
 import About from './components/pages/About';
 import ContactState from './context/contact/ContactState';
+import AuthState from './context/auth/AuthState';
 import './App.css';
 
 const App = () => {
   return (
-    <ContactState>
 
-    <Router>
+    <AuthState>
 
-      <Fragment>
-        <Navbar />
-        <div className="container">
-          <Routes>
-            <Route exact path='/' element = {(
-              <Fragment>
-                <Home />
-              </Fragment>
-            )} />
-            <Route exact path='/about' element = {(
-              <Fragment>
-                <About />
-              </Fragment>
-            )} />
-          </Routes>
-        </div>
-      </Fragment>
-    </Router>
-    </ContactState>
+      <ContactState>
+
+        <Router>
+
+          <Fragment>
+            <Navbar />
+            <div className="container">
+              <Routes>
+                <Route exact path='/' element={(
+                  <Fragment>
+                    <Home />
+                  </Fragment>
+                )} />
+                <Route exact path='/about' element={(
+                  <Fragment>
+                    <About />
+                  </Fragment>
+                )} />
+              </Routes>
+            </div>
+          </Fragment>
+        </Router>
+      </ContactState>
+    </AuthState>
 
 
 
