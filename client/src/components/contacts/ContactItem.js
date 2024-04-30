6ebe4597a38f92d4b354ffdbd0a73291ed6ed1e2
akashtripathi1 <120573423+akashtripathi1@users.noreply.github.com>
2024-04-30 15:21:00 +0530

    added delete contact functionality, lateron will also delete from the server database

--- client/src/components/contacts/ContactItem.js
+++ client/src/components/contacts/ContactItem.js
@@ -1,9 +1,20 @@
-import React from 'react';
-import PropTypes from 'prop-types'
+import React, { useContext } from 'react';
+import PropTypes from 'prop-types';
+import ContactContext from '../../context/contact/contactContext';
 
 
 const ContactItem = ({ contact }) => {
+
+    const contactContext = useContext(ContactContext);
+    const { deleteContact } = contactContext;
     const { id, name, email, phone, type } = contact;
+
+    const onDelete = () => {
+
+        deleteContact(id);
+
+    }
+
     return (
         <div className='card bg-light'>
             <h3 className="text-primary text-left">
@@ -27,7 +38,7 @@ const ContactItem = ({ contact }) => {
                 </li> )}
                 <p>
                     <button className="btn btn-dark btn-sm">Edit</button>
-                    <button className="btn btn-danger btn-sm">Delete</button>
+                    <button className="btn btn-danger btn-sm" onClick = {onDelete} >Delete</button>
                 </p>
                 
             </ul>
